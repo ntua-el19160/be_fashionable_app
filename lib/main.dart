0d01b439f4b007c00f5ba7df1e94ce5e0fb89af4
ntua-el19160 <el19160@mail.ntua.gr>
2023-01-06 12:31:54 +0200

    Main  Structure of bottom App Bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 void main() {
   runApp(const MyApp());
@@ -10,6 +11,50 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Be Fashionable');
+    return const MaterialApp(
+      title: 'Be Fashionable',
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        //body:
+        backgroundColor: const Color.fromARGB(255, 11, 2, 30),
+        bottomNavigationBar: BottomAppBar(
+            color: const Color.fromARGB(255, 5, 0, 17),
+            child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Padding(
+                  padding: const EdgeInsets.only(right: 40),
+                  child: CircleAvatar(
+                      radius: 30.0,
+                      backgroundColor: Colors.purple,
+                      child: IconButton(
+                        icon: const Icon(color: Colors.white, Icons.today),
+                        onPressed: () {},
+                      ))),
+              Padding(
+                  padding: const EdgeInsets.only(right: 40),
+                  child: CircleAvatar(
+                      radius: 30.0,
+                      backgroundColor: Colors.purple,
+                      child: IconButton(
+                        icon: const Icon(
+                            color: Colors.white, Icons.person_outline),
+                        onPressed: () {},
+                      ))),
+              CircleAvatar(
+                  radius: 30.0,
+                  backgroundColor: Colors.purple,
+                  child: IconButton(
+                    icon: const Icon(
+                        color: Colors.white, Icons.camera_alt_outlined),
+                    onPressed: () {},
+                  )),
+            ])));
   }
 }
